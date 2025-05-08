4887d46f02d13655851593be2669d37da6196780
MatheusCDias <matheuscdias2014@gmail.com>
2025-05-08 18:02:05 -0300

    Shopping_List: Completed

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,69 @@
+import { useRef, useState } from "react";
+import { v4 } from "uuid";
+import {
+  AddButton,
+  Container,
+  InputContainer,
+  ProductContainer,
+  Product,
+} from "./styles";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { IoIosAdd } from "react-icons/io";
+
+// React Hooks
+// useRef
+// useState / estado -> É uma variável que toda vez que troca de valor, a tela irá "recarregar os valores"
+
+function Home() {
+  const [products, setProducts] = useState([]);
+  const inputRef = useRef();
+
+  function buttonClick() {
+    const inputValue = inputRef.current.value.trim();
+
+    if (inputValue === "") return; // Evita adicionar itens vazios
+
+    setProducts([{ id: v4(), name: inputValue }, ...products]);
+    inputRef.current.value = "";
+  }
+
+
+  function deleteProduct(id) {
+    setProducts(products.filter((product) => product.id !== id));
+  }
+
+  function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      buttonClick();
+    }
+  }
+
+  return (
+    <Container>
+      <span>Nova</span>
+      <h1>Lista de Compras</h1>
+      <InputContainer>
+        <input
+          placeholder="Insira um produto..."
+          ref={inputRef}
+          onKeyDown={handleKeyDown}
+        />
+        <AddButton onClick={buttonClick}>
+          <IoIosAdd />
+        </AddButton>
+      </InputContainer>
+      <ProductContainer>
+        {products.map((products) => (
+          <Product key={products.id}>
+            <p>{products.name}</p>
+            <button onClick={() => deleteProduct(products.id)}>
+              <FaRegTrashAlt />
+            </button>
+          </Product>
+        ))}
+      </ProductContainer>
+    </Container>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home/styles.jsx
@@ -0,0 +1,155 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 40rem;
+  width: 30rem;
+  padding: 2.4rem;
+  gap: 0.8rem;
+  background-color: #fff;
+  border-radius: 1.2rem;
+  filter: drop-shadow(2px 8px 16px rgba(0, 0, 0, 0.25));
+
+  span {
+    height: 1.6rem;
+    overflow: hidden;
+    color: #999990;
+    white-space: nowrap;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    user-select: none;
+  }
+
+  h1 {
+    width: 13.5rem;
+    height: 8rem;
+    color: #333;
+    font-size: 3.2rem;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    user-select: none;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 0.8rem;
+  width: 25.2rem;
+  height: 4.8rem;
+  gap: 0.8rem;
+
+  input {
+    display: flex;
+    width: 19.6rem;
+    height: 4.8rem;
+    flex-shrink: 0;
+    padding: 0.8rem;
+    font-size: 1.6rem;
+    font-family: 'Roboto', sans-serif;
+    color: #333;
+    font-weight: 300;
+    border: none;
+    border-radius: 0.8rem;
+    outline: none;
+    background-color: #eee;
+  }
+
+  input::placeholder {
+    font-family: "Roboto", sans-serif;
+    color: #aaa;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 4.8rem;
+  height: 4.8rem;
+  aspect-ratio: 1/1;
+  border: none;
+  border-radius: 40rem;
+  cursor: pointer;
+  user-select: none;
+  background: linear-gradient(315deg, #2d9963 0%, #50cc71 100%);
+  color: #FFF;
+  font-size: 32px;
+  will-change: transform;
+  transition: transform 0.5s ease-in-out, filter 0.5s ease-in-out;
+
+  &:hover {
+    transform: scale(1.05);
+    filter: drop-shadow(2px 8px 16px rgba(0, 0, 0, 0.25));
+  }
+
+`;
+
+export const ProductContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 1.6rem;
+  max-height: 16rem;
+  width: 26rem;
+  gap: 0.8rem;
+  overflow-y: auto;
+`;
+
+export const Product = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 25.2rem;
+  height: 4.8rem;
+  background-color: #eee;
+  border-radius: 0.8rem;
+  transition: background-color 0.5s ease-in-out;
+
+  p {
+    display: flex;
+    align-items: center;
+    width: 19.6rem;
+    height: 100%;
+    border-radius: 0.8rem;
+    padding: 0.8rem;
+    background-color: #eee;
+
+    color: #444;
+    font-family: Roboto;
+    font-size: 1.6rem;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+  }
+
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 4.8rem;
+    height: 4.8rem;
+    aspect-ratio: 1/1;
+    background-color: transparent;
+    border: none;
+    border-radius: 0.8rem;
+    cursor: pointer;
+    color: #444;
+    font-size: 24px;
+    user-select: none;
+    transition: background-color 0.5s ease-in-out, color 0.5s ease-in-out;
+
+    &:hover {
+      background-color: #444;
+      color: #fff;
+    }
+  }
+`;
